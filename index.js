570ca1a9c8f17244101bcc854ad9c412eed6fb1f
rsach <rahul.sachdeva@live.com>
2017-07-25 17:07:54 +0530

    redirect

---
+++ index.js
@@ -0,0 +1,7 @@
+window.location.replace('https://rahulsachdeva.in');
+
+// use this to redirect, a back button call will trigger the redirection again
+window.location.href = "https://rahulsachdeva.in";
+
+// given for completeness, essentially an alias to window.location.href
+window.location = "https://rahulsachdeva.in";
